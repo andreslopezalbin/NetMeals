0ff363641a7ba22d69d2a0100afc3e7a9254c23d
Andrés López Albín <andres.albin93@gmail.com>
2017-03-14 23:07:26 +0100

    Creado populate inicial y añadidos los roles de los usuarios monitor, manager y chef

--- netmeals/netmeals/settings.py
+++ netmeals/netmeals/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'core',
     'recommendation_system',
-    'activities'
+    'activities',
+    'netmeals'
 ]
 
 MIDDLEWARE = [
@@ -126,7 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-APPEND_SLASH=False
+APPEND_SLASH = False
 
 # LOGIN
 LOGIN_URL = "/login"
