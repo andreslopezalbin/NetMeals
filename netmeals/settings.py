f1cf93415fffd01fad98e3e150711f3b496b308d
Andrés López Albín <andres.albin93@gmail.com>
2017-05-22 17:12:45 +0200

    quitado static dirs

--- netmeals/settings.py
+++ netmeals/settings.py
@@ -147,11 +147,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 APPEND_SLASH = False
 
