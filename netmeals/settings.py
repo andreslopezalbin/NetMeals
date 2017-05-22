4357d9d0b60b818c194638cf61b1e6c0ed1e095e
Andrés López Albín <andres.albin93@gmail.com>
2017-05-22 17:57:34 +0200

    pruebas para deploy 2

--- netmeals/settings.py
+++ netmeals/settings.py
@@ -147,7 +147,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
