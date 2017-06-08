a50defc79729efc109a96a5cdb5a3fc7cd7494a1
Andrés López Albín <andres.albin93@gmail.com>
2017-06-08 21:37:27 +0200

    Añadida libreria Materialize para Bootstrap Cambiado navbar, login, footer, vista scheduler, listado de platos

--- activities/views/dish_view.py
+++ activities/views/dish_view.py
@@ -3,7 +3,7 @@ from activities.forms.DishForm import DishForm
 from activities.models import Dish
 import json
 from datetime import date
-
+from django.core.exceptions import ObjectDoesNotExist
 
 def findall(request):
     if request.method == "GET":
@@ -26,10 +26,6 @@ def schedule(request):
         dishes = request.user.guest.chef.dish_set.all()
         user_language = request.LANGUAGE_CODE
         today = date.today()
-        # title: 'Click for Google',
-        # url: 'http://google.com/',
-        # start: '2017-05-28 21:10',
-        # color: '#257e4a'
         items = []
         for dish in dishes:
             item = {'title': dish.name,
@@ -54,16 +50,22 @@ def details(request, dish_id):
 
 
 def delete(request, dish_id):
-    dish = Dish.objects.get(id=dish_id)
-    if request.user.groups.filter(name='Chef').exists() and dish.owner == request.user.guest.chef:
-        dish.delete()
+    try:
+        dish = Dish.objects.get(id=dish_id)
+        if request.user.groups.filter(name='Chef').exists() and dish.owner == request.user.guest.chef:
+            dish.delete()
+            dishes = request.user.guest.chef.dish_set.all()
+            context = {'dishes': dishes, 'deleted': True}
+            return render(request, '../templates/dish/list.html', context)
+        else:
+            available_seats = dish.max_assistants - len(dish.assistants.all())
+            context = {'dish': dish, 'available_seats': range(0, available_seats), 'delete_error': True}
+            return render(request, '../templates/dish/details.html', context)
+    except ObjectDoesNotExist:
         dishes = request.user.guest.chef.dish_set.all()
-        context = {'dishes': dishes, 'deleted': True}
+        context = {'dishes': dishes, 'error': True}
         return render(request, '../templates/dish/list.html', context)
-    else:
-        available_seats = dish.max_assistants - len(dish.assistants.all())
-        context = {'dish': dish, 'available_seats': range(0, available_seats), 'delete_error': True}
-        return render(request, '../templates/dish/details.html', context)
+
 
 
 def edit_dish(request):
@@ -85,28 +87,7 @@ def edit_dish(request):
         }
     return render(request, 'dish/edit.html', context)
 
-    # @method_decorator(group_required('Chef'), name='dispatch')
-    # class EditDish(View):
-    #     def get(self, request):
-    #         form = DishForm()
-    #         context = {
-    #             'form': form,
-    #         }
-    #         return render(request, 'dish/edit.html', context)
-    #
-    #     def post(self, request):
-    #         form = DishForm(request.POST)
-    #         if form.is_valid():
-    #             dish = form.create(request)
-    #             self.save(dish);
-    #             return redirect("my_dishes")
-    #         else:
-    #             context = {
-    #                 'form': form,
-    #             }
-    #             return render(request, 'dish/edit.html', context)
-    #
-    #     def save(self, dish):
-    #         if not dish.photo:
-    #             dish.photo = "/static/images/dish-food-1.jpg"
-    #         dish.save()
+
+def create_without_login(request):
+    if request.method == 'GET':
+        return render(request, '../templates/dish/create_w_login.html')
